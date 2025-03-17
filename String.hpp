a72fc57246c5b89cdcfa508e02bf51abeee26441
arturmatevosyan99 <60891912+arturmatevosyan99@users.noreply.github.com>
2025-03-17 12:56:54 +0400

    CAP-604 #feature

--- String.hpp
+++ String.hpp
@@ -33,6 +33,20 @@ namespace Qaos {
 		}
 
 		static QString CamelCase(const QString& input, const QSet<QString>& char_set = {"_"});
+
+		static inline bool IsInteger(const QString& s)
+		{
+			if (s.isEmpty()) {
+				return false;
+			}
+			else if (s.front() == '-' || s.front() == '+') {
+				return (s.length() > 1) &&
+					   (std::find_if(s.begin() + 1, s.end(), [](QChar c) { return !c.isDigit(); }) == s.end());
+			}
+			else {
+				return std::find_if(s.begin(), s.end(), [](QChar c) { return !c.isDigit(); }) == s.end();
+			}
+		}
 	/** @} */
 
 	/** @name Constructors */
